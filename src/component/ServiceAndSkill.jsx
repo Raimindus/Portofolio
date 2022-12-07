edb0f22b194f07739621024eea2c274eae94aa17
Raimindus <draimindus@gmail.com>
2022-12-07 17:16:06 +0700

    add vanilla-tilt

--- src/component/ServiceAndSkill.jsx
+++ src/component/ServiceAndSkill.jsx
@@ -1,19 +1,20 @@
 import { CardImg } from "react-bootstrap";
 import Card from "react-bootstrap/Card";
 import style from "../css/ServiceAndSkill.module.css";
-
-// import style from "../css/ServiceAndSkill.module.css";
+import Tilt from "react-vanilla-tilt";
 
 function skill({ image, title }) {
   return (
     <div className={style.container}>
       <Card className={style.card}>
-        <CardImg src={image} />
-        <Card.Body>
-          <Card.Title>
-            <div>{title}</div>
-          </Card.Title>
-        </Card.Body>
+        <Tilt className={style.tilt}>
+          <CardImg src={image} />
+          <Card.Body>
+            <Card.Title>
+              <div>{title}</div>
+            </Card.Title>
+          </Card.Body>
+        </Tilt>
       </Card>
     </div>
   );
