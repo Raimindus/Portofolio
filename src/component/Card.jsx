7407b643b18ffce17db0ecb1dbca225d4119bcfc
Raimindus <draimindus@gmail.com>
2022-12-10 04:48:21 +0700

    adding glare effect function

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,12 +1,30 @@
 import { CardImg } from "react-bootstrap";
 import Card from "react-bootstrap/Card";
 import style from "../css/Card.module.css";
-import Tilt from "react-vanilla-tilt";
+import VanillaTilt from "vanilla-tilt";
+import { useEffect, useRef } from "react";
+
+function Tilt(props) {
+  const { options, ...rest } = props;
+  const tilt = useRef(null);
+
+  useEffect(() => {
+    VanillaTilt.init(tilt.current, options);
+  }, [options]);
+
+  return <div ref={tilt} {...rest} />;
+}
 
 function skill({ image, title }) {
+  const options = {
+    speed: 1000,
+    max: 30,
+    glare: true,
+    "max-glare": 1,
+  };
   return (
     <div className={style.container}>
-      <Tilt className={style.tilt} options={{ glare: true, "max-glare": 1 }}>
+      <Tilt className={style.tilt} options={options}>
         <Card className={style.card}>
           <div className={style.content}>
             <CardImg src={image} />
