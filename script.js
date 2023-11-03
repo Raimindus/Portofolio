0451e1817874583235d491c25a2784e2cadcc594
Raimindus <draimindus@gmail.com>
2023-11-03 15:41:59 +0700

    Porto

---
+++ script.js
@@ -0,0 +1,78 @@
+/*==========scroll sections active link==========*/
+
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+  /*==========sticky Navbar==========*/
+
+  let header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 100);
+
+  /*==========remove toggle icon and navbar when clicked navbar link(Scroll)==========*/
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
+};
+
+/*==========toggle Navbar icon==========*/
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
+
+/*==========scroll Reveal==========*/
+ScrollReveal({
+  reset: true,
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
+
+ScrollReveal().reveal(".home-content, .heading", { origin: "right" });
+ScrollReveal().reveal(
+  ".home-img, .services-container, .portfolio-box, .contact form",
+  {
+    origin: "bottom",
+  }
+);
+ScrollReveal().reveal(".home-content, about-img", {
+  origin: "left",
+});
+ScrollReveal().reveal(".home-content p, about-content", {
+  origin: "top",
+});
+
+/*==========typed js==========*/
+const typed = new Typed(".multiple-text", {
+  strings: ["Frontend Developer"],
+  typeSpeed: 140,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
+
+const readMore = document.getElementById("read-more-button");
+const overflowText = document.querySelector(".overflow-text p");
+readMore.addEventListener("click", function () {
+  overflowText.style.overflow = "visible";
+  overflowText.style.whiteSpace = "normal";
+  readMore.style.display = "none";
+});
